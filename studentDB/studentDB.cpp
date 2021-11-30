10c574da82e477650721007819129259b53eeba2
P0lskay <gg.lol.2264@gmail.com>
2021-11-30 23:27:06 +0300

    Дописан текстовый интерфейс приложения для добавления студента в список, в studentDB. Однако, пока сравнение строк не работает корректно

--- studentDB/studentDB.cpp
+++ studentDB/studentDB.cpp
@@ -5,15 +5,19 @@
 
 int main()
 {
-	Student* m = new Student("Andrew", "Male", 19, 2, 2, { {"Диференицальные уравнения", 4} }, { {"Экономика", false}});
-	
+
 	setlocale(LC_ALL, "Russian");
+	Student test;
+	Student* m = new Student("Андрей", "м", 19, 2, 2, { {"Диференицальные уравнения", 4} }, { {"Экономика", false}});
+	
 
 	std::string chois;
 	std::string name;
 	int age;
 	int group;
 	int course;
+	int ex_size;
+	int test_size;
 	std::string gender;
 
 	std::cout << "Введите: \"студент\", если хотите добавить студента, либо введите: \"преподаватель\", если хотите добавить преподователя, если хотите выйти из программы, то введите q и нажмите Enter: ";
@@ -28,12 +32,45 @@ int main()
 		std::cout << "Возраст: ";
 		std::cin >> age;
 
+
 		if (chois == "студент")
 		{
 			std::cout << "Курс студента: ";
 			std::cin >> course;
 			std::cout << "Номер группы студента: ";
 			std::cin >> group;
+			
+			std::cout << "Количество экзаменов: ";
+			std::cin >> ex_size;
+			std::map <std::string, int> ex_grades;
+			for (int i = 0; i < ex_size; i++)
+			{
+				std::string lesson;
+				int grade;
+				std::cout << "Экзаминационный предмет: ";
+				std::cin >> lesson;
+				std::cout << "Оценка: ";
+				std::cin >> grade;
+				ex_grades[lesson] = grade;
+			}
+			std::cout << "Количество зачетов: ";
+			std::cin >> test_size;
+			std::map <std::string, bool> test_grades;
+			for (int i = 0; i < test_size; i++)
+			{
+				std::string lesson;
+				std::string res;
+				std::cout << "Предмет: ";
+				std::cin >> lesson;
+				std::cout << "Введите Y, если предмет зачтен или N, если нет: ";
+				std::cin >> res;
+				if (res == "Y" || res == "Yes")
+					test_grades[lesson] = true;
+				else
+					test_grades[lesson] = false;
+			}
+
+			Student* student = new Student(name, gender, age, course, group, ex_grades, test_grades);
 
 		}
 		else if (chois == "преподаватель")
@@ -44,6 +81,16 @@ int main()
 		{
 
 		}
+
+		auto students_list = test.getStudents();
+
+		for (Student student : students_list)
+		{
+			std::cout << student.getName() << "; ";
+		}
+
+		std::cout << "Введите: \"студент\", если хотите добавить студента, либо введите: \"преподаватель\", если хотите добавить преподователя, если хотите выйти из программы, то введите q и нажмите Enter: ";
+		std::cin >> chois;
 	}
 }
 
